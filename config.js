d8b6bd8863896b252ec579475054fb6d787943c5
PuRgE-CoDeE <69874895+PuRgE-CoDeE@users.noreply.github.com>
2020-12-25 13:54:18 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,43 @@
+<script type="text/javascript">
+var deadline = '1 Jan 2021';
+
+function getTimeRemaining(endtime){
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    console.log(t);
+    var seconds = Math.floor( (t/1000) % 60 );
+    var minutes = Math.floor( (t/1000/60) % 60 );
+    var hours = Math.floor( (t/(1000*60*60)) % 24 );
+    var days = Math.floor( t/(1000*60*60*24) );
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function initializeClock(id, endtime){
+    var clock = document.getElementById(id);
+    function updateClock(){
+        var t = getTimeRemaining(endtime);
+        var daysSpan = clock.querySelector('.days');
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = t.hours;
+        minutesSpan.innerHTML = t.minutes;
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+        if(t.total<=0){
+                      window.location.href="index.html";
+        }
+
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock,1000);
+}
+
+initializeClock('clockdiv', deadline);
+</script>
